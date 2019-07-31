1bb5dc9edf70674198c14bfbfb5a0057d4b033b1
Adriano de Souza <adrianosouza.ms@hotmail.com>
2019-07-31 17:17:51 -0300

    up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ProductBoxComponent } from './product-box/product-box.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from './product-box/product-box.service';
 import { SharedModule } from './shared/shared.module';
+import { ProductService } from './service/product-box.service';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductBoxComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
